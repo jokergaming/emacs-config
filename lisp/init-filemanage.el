2037d650c9c63477b0495c719c2e9c7c4573ecd2
joker <jokergaming@163.com>
2022-01-27 02:23:42 +0800

    上一次补充

---
+++ lisp/init-filemanage.el
@@ -0,0 +1,16 @@
+(setq dired-recursive-deletes 'always)
+(setq dired-recursive-copies 'always)
+
+(put 'dired-find-alternate-file 'disabled nil)
+
+;; 主动加载 Dired Mode
+;; (require 'dired)
+;; (defined-key dired-mode-map (kbd "RET") 'dired-find-alternate-file)
+
+;; 延迟加载
+(with-eval-after-load 'dired
+    (define-key dired-mode-map (kbd "RET") 'dired-find-alternate-file))
+
+(require 'dired-x)
+(setq dired-dwin-target 1)
+(provide 'init-filemanage)
---
+++ lisp/init-org.el
@@ -0,0 +1,5 @@
+(require 'org)
+
+(setq org-src-fontify-natively t)
+
+(provide 'init-org)
