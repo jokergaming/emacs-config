adabb14ba1fe69f1f4d4f0a042a9717bc41cc620
joker <jokergaming@163.com>
2022-03-29 21:20:02 +0800

    修改init-haskell

--- lisp/init-haskell.el
+++ lisp/init-haskell.el
@@ -14,5 +14,13 @@
   '(haskell-process-log t))
 
 (define-key haskell-mode-map (kbd "C-c C-l") 'haskell-process-load-or-reload)
+(define-key haskell-mode-map (kbd "C-`") 'haskell-interactive-bring)
+(define-key haskell-mode-map (kbd "C-c C-t") 'haskell-process-do-type)
+(define-key haskell-mode-map (kbd "C-c C-i") 'haskell-process-do-info)
+(define-key haskell-mode-map (kbd "C-c C-c") 'haskell-process-cabal-build)
+(define-key haskell-mode-map (kbd "C-c C-k") 'haskell-interactive-mode-clear)
+
+
+
 
 (provide 'init-haskell)
